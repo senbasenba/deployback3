74ba9a524e70b3c58d9eadd28a8ec48b8d954795
senbasenba <senba.senba19@gmail.com>
2024-11-09 14:44:00 +0900

    no message

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})  # CORS設定を更新
+CORS(app, resources={r"/api/*": {"origins": "ttps://temp-step32-demo-back-ajdzemd0fyfzapff.eastus-01.azurewebsites.net"}})  # CORS設定を更新
 
 @app.route('/', methods=['GET'])
 def hello():
